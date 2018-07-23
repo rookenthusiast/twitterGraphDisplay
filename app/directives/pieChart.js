2c2e4baa59ddbaebf07b0cdeef7b3be94461f388
cameron <cameron.fulton@financial-cloud.com>
2018-07-23 01:24:32 +0100

    svg is being appended on init alongside paths, however it is not displaying"

--- app/directives/pieChart.js
+++ app/directives/pieChart.js
@@ -5,14 +5,19 @@ app.directive('pieChart', [function () {
             data: '='
         },
         link: function (scope, element, attrs) {
-            element[0].append(document.createElement('svg'));
+            var chart = document.createElement('svg');
+            chart.setAttribute('width',960);
+            chart.setAttribute('height',500);
+            element[0].append(chart);
             var svg = d3.select('svg'),
-                width = svg.attr("width"),
-                height = svg.attr("height"),
+                width = svg.attr('width'),
+                height = svg.attr('height'),
                 radius = Math.min(width, height) / 2,
                 g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
             var color = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
+            console.log(width);
+            console.log(height);
+            console.log(radius);
             // Generate the pie
             var pie = d3.pie();
 
@@ -31,6 +36,7 @@ app.directive('pieChart', [function () {
             //Draw arc paths
             arcs.append("path")
                 .attr("fill", function (d, i) {
+                    console.log(i);
                     return color(i);
                 })
                 .attr("d", arc);
@@ -43,16 +49,7 @@ app.directive('pieChart', [function () {
                 console.log(pie([firstLength, secondLength]))
                 var path = svg.selectAll('path');
                 path.data(pie([firstLength, secondLength]));
-                path.transition().duration(750).attrTween("d", arcTween); // redraw the arcs
-            }, true)
-
-            function arcTween(a) {
-                var i = d3.interpolate(this._current, a);
-                this._current = i(0);
-                return function (t) {
-                    return arc(i(t));
-                };
-            }
+            }, true);
         }
     }
 }]);
